127c8fba4e5205d71897fb912e68675af37a2789
Brandon Kelly <bkelly8@clemson.edu>
2024-02-01 21:04:00 -0500

    Removed Workspace files

--- assignment2_ws/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brandon/AuE_8230/git_ws/AuE8230Spring24_BrandonKelly/assignment2_ws/src
-# Build directory: /home/brandon/AuE_8230/git_ws/AuE8230Spring24_BrandonKelly/assignment2_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("turtle_circle")
-subdirs("turtle_square_openloop")
--- assignment2_ws/build/turtle_square_openloop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brandon/AuE_8230/git_ws/AuE8230Spring24_BrandonKelly/assignment2_ws/src/turtle_square_openloop
-# Build directory: /home/brandon/AuE_8230/git_ws/AuE8230Spring24_BrandonKelly/assignment2_ws/build/turtle_square_openloop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
